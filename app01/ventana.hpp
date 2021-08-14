3a5556277557027590ebfb20f600c57a9121c84e
Kevin Bohorquez <kevintor2246@gmail.com>
2021-08-14 13:18:14 -0500

    Update ventana.hpp

--- app01/ventana.hpp
+++ app01/ventana.hpp
@@ -7,16 +7,16 @@ class Ventana {
 
 protected:
 
-	static std::shared_ptr<const char*>nombre;  /* window name */
-	static std::shared_ptr<GLFWwindow*> ventana; /* GLFWwindow* instance */
-	static GLuint alto, ancho; /* window size and name */
+	static std::shared_ptr<const char*>nombre;  			/* window name */
+	static std::shared_ptr<GLFWwindow*> ventana; 			/* GLFWwindow* instance */
+	static GLuint alto, ancho; 					/* window size and name */
 
 public:
 
-	Ventana(const char*); /* constructor whith window just the name */
-	static void init();  /*this in main.cpp is so important  */
-	static void error_callback(int, const char*); /*control funcs */
-	static void key_callback(GLFWwindow*, int, int, int, int); /*control funcs */
+	Ventana(const char*);					 	/* constructor whith window just the name */
+	static void init();  				 	 	/*this in main.cpp is so important  */
+	static void error_callback(int, const char*); 	          	/*control funcs */
+	static void key_callback(GLFWwindow*, int, int, int, int);	/*control funcs */
 
 };
 
